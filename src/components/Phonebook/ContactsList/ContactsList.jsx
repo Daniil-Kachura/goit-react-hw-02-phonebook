2907bd0b73b2268eb6eaf666b1049522c434bf6c
Daniil-Kachura <ka4urad@gmail.com>
2022-10-11 00:56:31 +0300

    phonebook

--- src/components/Phonebook/ContactsList/ContactsList.jsx
+++
@@ -1,8 +0,0 @@
-export default function ContactsList({ items, removeContact }) {
-  const elements = items.map(({ name, number, id }) => {
-    return <li className="contacts-item" key={id}>{name}: {number}<span className="delete-btn" onClick={() => removeContact(id)}>Delete</span></li>
-  })
-  return (
-    <ul className="contacts-list">{elements}</ul>
-  )
-}
